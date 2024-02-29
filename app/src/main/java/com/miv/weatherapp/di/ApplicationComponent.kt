b142874be8e128829d0721e60a4eee431f7bc499
miv-dev <i@mike39.ru>
2024-02-29 23:54:22 +0300

    Add FavouriteStore

--- app/src/main/java/com/miv/weatherapp/di/ApplicationComponent.kt
+++ app/src/main/java/com/miv/weatherapp/di/ApplicationComponent.kt
@@ -6,7 +6,7 @@ import dagger.Component
 
 @ApplicationScope
 @Component(
-    modules = [DataModule::class]
+    modules = [DataModule::class, PresentationModule::class]
 )
 interface ApplicationComponent {
 
---
+++ app/src/main/java/com/miv/weatherapp/di/PresentationModule.kt
@@ -0,0 +1,17 @@
+package com.miv.weatherapp.di
+
+import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
+import dagger.Module
+import dagger.Provides
+
+@Module
+interface PresentationModule {
+    companion object{
+        @Provides
+        fun provideStoreFactory(): StoreFactory = DefaultStoreFactory()
+
+    }
+
+
+}
